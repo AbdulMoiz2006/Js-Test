41eda23dcdbf096838858b69e4e4de36f7fcb669
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-10-11 18:50:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+
+
+// var Name = prompt("Enter your Name");
+// var age = prompt("Enter your Age");
+
+
+// alert(`Hello, ${Name}! you are ${age} years old`);
+
+
+
+// var num = +prompt("Enter your number")
+// if(num > 0){
+// alert("The number is positive");
+// }
+// else if(num < 0 ){
+//     alert("The number is negative")
+
+// }
+// else if(num === 0){
+
+// alert("The number is zero")
+
+// }
+
+
+// var std1 = +prompt("Enter your first number")
+// var std2 = +prompt("Enter your Second number")
+// var std3 = +prompt("Enter your third number")
+// var std4 = +prompt("Enter your fourth number")
+// var std5 = +prompt("Enter your Fifth number")
+
+// var sum = std1 + std2 + std3 + std4 + std5;
+
+// var average = sum / 5;
+
+// alert(`The average of the five number is ${average}`);
+
+
+
+// var city = ["karachi","Lahore","islamabad","Hyderabad","kpk"]
+
+// for (let i = 0; i < city.length; i++) {
+//     console.log(city[i]);
+    
+// }
